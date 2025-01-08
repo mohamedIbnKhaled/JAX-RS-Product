6fd76d4e9cf0cfb1580f67ab2d63aeac88947dc5
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-01-08 21:34:19 +0200

    add delete and update methond method

--- src/main/java/com/example/jakarta/hello/ProductResource.java
+++ src/main/java/com/example/jakarta/hello/ProductResource.java
@@ -21,12 +21,31 @@ public class ProductResource {
     }
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-
     @Path("/search")
     public  Response getProduct(@QueryParam("name") String name){
         Product product;
         product =  productManager.searchProduct(name);
         if(product==null)return  Response.status(Response.Status.BAD_REQUEST).build();
         return Response.status(Response.Status.ACCEPTED).entity(product).build();
+    }
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/delete")
+    public Response deleteProduct(Product product){
+        productManager.deleteProduct(product);
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/update")
+    public Response updateProduct(@QueryParam("name") String name,@QueryParam("newName") String newName,@QueryParam("price") double price){
+        try {
+            productManager.updateProduct(name,newName,price);
+        } catch (Exception e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+        return Response.status(Response.Status.ACCEPTED).build();
     }
 }
